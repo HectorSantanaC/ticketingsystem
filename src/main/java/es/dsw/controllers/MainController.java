bfef3ec66f06bbeacbd42f1dc0131405082017ed
‘alumno-ets’ <alumno-ets@alumno.ieselrincon.es>
2025-10-05 12:52:08 +0100

    Añadido slogan del dia y precio

--- src/main/java/es/dsw/controllers/MainController.java
+++ src/main/java/es/dsw/controllers/MainController.java
@@ -1,13 +1,30 @@
 package es.dsw.controllers;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import es.dsw.model.Cine;
 
 @Controller
 public class MainController {
 	
 	@GetMapping(value= {"/", "/index"})
-	public String idx() {
+	public String idx(Model cine) {
+		// Determinar el día
+		DayOfWeek dia = LocalDate.now().getDayOfWeek();
+		
+		// Obtener el slogan
+		String mensaje = Cine.getSloganDelDia(dia);
+		String precio = Cine.getPrecio(dia);
+		
+		// Pasarlo a la vista
+		cine.addAttribute("mensaje", mensaje);
+		cine.addAttribute("precio", precio);
+		
 		return "index";
 	}
 	
